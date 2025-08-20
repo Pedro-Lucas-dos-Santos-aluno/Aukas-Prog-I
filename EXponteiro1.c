69d5bbfd62bbfad99299a738ef7b34e155e5f2f9
Pedro Lucas dos Santos <pedrolucas18756@gmail.com>
2025-08-20 14:02:07 +0000

    att

--- EXponteiro1.c
+++ EXponteiro1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char frase[] = "não gosto de programar em C";
+    char frase[] = "nao gosto de programar em C";
     char *p = frase + 4;
     printf("%s\n", p);
     return 0;
